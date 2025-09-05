3c4bd84f40d697e44cb0600db6cd76ece855d52f
Minkyu Kim <82827785+minkyu307@users.noreply.github.com>
2025-09-05 11:31:31 +0900

    web, was init

---
+++ was/src/test/java/com/minkyuKim/autoCodeReview/AutoCodeReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minkyuKim.autoCodeReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCodeReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
